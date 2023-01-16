054580aadd6415e9d225411bb58cdb32315c129c
fatih d <abc@gmail.com>
2023-01-16 14:01:44 +0300

    all file added

---
+++ src/Task1/Task1Class.java
@@ -0,0 +1,7 @@
+package Task1;
+
+public class Task1Class {
+    public static void main(String[] args) {
+        System.out.println("AuditGitPractice Task1");
+    }
+}
---
+++ src/Task1_2/Task1_2Class.java
@@ -0,0 +1,7 @@
+package Task1_2;
+
+public class Task1_2Class {
+    public static void main(String[] args) {
+        System.out.println("AuditGitPractice Task1 Task1_2 Class");
+    }
+}
